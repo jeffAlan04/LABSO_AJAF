6c3fbe2eff5ed184ed122e2643fe17e067860c69
jeffAlan04 <jeffalan.shuyinta@studio.unibo.it>
2025-07-24 13:15:20 +0200

    add: connessione al master

--- src/client/Client.java
+++ src/client/Client.java
@@ -1,5 +1,8 @@
 import java.io.*;
+import java.net.*;
 import java.util.Scanner;
+
+//alMaster = outputMaster
 
 public class Client {
   public static void main(String[] args) {
@@ -11,12 +14,21 @@ public class Client {
     String indirizzoMaster = args[0];
     int porta = Integer.parseInt(args[1]);
 
-    Scanner tastiera = new Scanner(System.in);
+    try(
+      Socket socket = new Socket(indirizzoMaster, porta);
+      Scanner inputMaster = new Scanner(socket.getInputStream());
+      PrintWriter outputMaster = new PrintWriter(socket.getOutputStream());
+      Scanner tastiera = new Scanner(System.in);
+    ){
+      System.out.println("Connessione al master: " + socket.getRemoteSocketAddress());
 
-    while (true) {
-      System.out.print("> ");
-      String inputUtente = tastiera.nextLine();
+      while (true) {
+        System.out.print("> ");
+        String inputUtente = tastiera.nextLine();
+      }
     }
-
+    catch(IOException e){
+      System.out.println("Errore di connessione al master");
+    }
   }
 }
