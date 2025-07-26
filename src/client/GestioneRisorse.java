615e68460e7a35f2862117c9ecb64ec648cae485
jeffAlan04 <jeffalan.shuyinta@studio.unibo.it>
2025-07-26 13:42:19 +0200

    add: listdata local a GestioneRisorse.java

--- src/client/GestioneRisorse.java
+++ src/client/GestioneRisorse.java
@@ -1,4 +1,5 @@
 import java.io.File;
+import java.io.PrintWriter;
 import java.util.LinkedList;
 
 public class GestioneRisorse {
@@ -20,5 +21,22 @@ public class GestioneRisorse {
     }
 
     return "false";
+  }
+// Metodo per il comando listdata local
+  public static void eseguiListDataLocal(){
+    File input = new File("risorse");
+    System.out.println("Path assoluto: " + input.getAbsolutePath());
+    File[] risorse = input.listFiles();
+
+    if (risorse == null || risorse.length == 0){
+      System.out.println("Nessuna risorsa disponibile");
+      return;
+    }
+    System.out.println("Risorse: ");
+    for (File f : risorse) {
+      if(f.isFile()){
+        System.out.println("- " + f.getName());
+      }
+    }
   }
 }
