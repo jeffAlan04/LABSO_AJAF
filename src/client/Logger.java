c8441b06e77106d29a11f8f0d71866ad278c3673
Filippo Bucciarelli <87319226+aka-buccia@users.noreply.github.com>
2025-08-08 22:34:20 +0200

    add: creazione file di log

--- src/client/Logger.java
+++ src/client/Logger.java
@@ -1,12 +1,27 @@
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class Logger {
   private static PrintWriter scrittore;
 
   public Logger() {
-    // Creazione File di log
+    try {
+      FileWriter f = new FileWriter(new File(generaNome()), true);
+      scrittore = new PrintWriter(f);
+    } catch (IOException e) {
+      // Rilancia l'eccezione sul chiamante così da interrompere l'esecuzione
+      throw new RuntimeException("Errore durante la creazione di un file di log", e);
+    }
+  }
+
+  public static String generaNome() {
+    LocalDateTime momento = LocalDateTime.now();
+    DateTimeFormatter formattatore = DateTimeFormatter.ofPattern("yyyy-MM-dd_HH-mm-ss");
+    String dataOra = momento.format(formattatore);
+    return "log_" + dataOra + ".txt";
   }
 
   public static void log(String messaggio) {
