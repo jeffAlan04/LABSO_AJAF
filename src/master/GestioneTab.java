934f9e53fa7a96d7b8e7d7c792cbd32226de8ad5
AnouarElKihal <elkihalanouar@gmail.com>
2025-08-14 19:37:25 +0200

    add: cartella risorse e file gestione tabella

---
+++ src/master/GestioneTab.java
@@ -0,0 +1,19 @@
+public class GestioneTab {
+    // 1. Creazione tabella JSON (es: {R1: P1,P2; R2: P1, P3; R3: P2})
+    
+
+    // 2. "listdata remote": getRisorse()
+
+
+    // 3. "add": addRisorsa()
+
+
+    // 4. "log": getLog() (es: "13:00 R0 da: peer0 a: peer1")
+
+
+    // 5. addPeer()
+
+
+    // 6. removePeer()
+}
+
