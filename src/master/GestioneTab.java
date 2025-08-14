b465139cf3bbb2a6515f0c7d4fedd1148d01399b
AnouarElKihal <elkihalanouar@gmail.com>
2025-08-15 00:48:24 +0200

    add: setup iniziale dei metodi necessari per il funzionamento della tabella

--- src/master/GestioneTab.java
+++ src/master/GestioneTab.java
@@ -1,19 +1,64 @@
+import java.util.*;
+
 public class GestioneTab {
-    // 1. Creazione tabella JSON (es: {R1: P1,P2; R2: P1, P3; R3: P2})
-    
+    private Map<String, List<String>> risorse = new HashMap<>();
 
-    // 2. "listdata remote": getRisorse()
+    // GET
+    public List<String> getRisorse() {
+        // Restituisci tutte le risorse (comando `listdata remote`)
+    }
 
-
-    // 3. "add": addRisorsa()
-
-
-    // 4. "log": getLog() (es: "13:00 R0 da: peer0 a: peer1")
-
-
-    // 5. addPeer()
+    public List<String> getPeers(String risorsa) {
+        // Restituisci tutti peer che hanno quella risorsa
+    }
 
 
-    // 6. removePeer()
+    // AGGIUNTA
+    public void aggiungiRisorsa(String risorsa, List<String> indirizziIp) {
+        // Controlla che la risorsa esista
+        // se non esiste la crei
+    }
+
+    public void aggingiPeer(String indirizzoIp, List<String> risorse) {
+        // Per ogni risorsa del peer da aggiungere chiamare metodo `aggiungiRisorsa()` 
+        // che aggiunge questo peer alla lista di ogni risorsa che contiene
+    }
+
+
+    // RIMOZIONE
+    public String rimuoviPeer(String ipAddress) {
+        if (...) {
+            return "Rimozione peer avvenuta con successo!";
+        }
+        else {
+            return "Rimozione peer fallita.";
+        }
+    }
+
+    public String rimuoviRisorsa(String risorsa) {
+        if (...) {
+            return "Rimozione risorsa avvenuta con successo!";
+        }
+        else {
+            return "Rimozione risorsa fallita.";
+        }
+    }
+
+
+    // SALVATAGGIO E CARICAMENTO DATI
+    public void caricaDaFile() {
+        
+    }
+
+    public void salvaSuFile() {
+
+    }
+
+
+    public static void main(String[] args) {
+        // fare test
+    }
 }
+
+
 
