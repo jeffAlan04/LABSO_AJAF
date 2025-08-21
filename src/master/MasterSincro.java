e743b21c7fb3a06927d1cfdf37627e89899a0228
james <jamesngoufack@gmail.com>
2025-08-21 05:04:57 +0200

    docs: Aggiunta dei commenti

--- src/master/MasterSincro.java
+++ src/master/MasterSincro.java
@@ -1,51 +1,61 @@
 package master;
 
-public class MasterSincro{
+// Classe che implementa un meccanismo di sincronizzazione lettori-scrittori
+// permette più lettori simultanei ma scrittori esclusivi
+public class MasterSincro {
 
-private int lettoriAttivi;
-private boolean inLettura;
-private boolean inScrittura;
+    private int lettoriAttivi;  // numero di lettori attivi
+    private boolean inLettura;  // true se ci sono lettori attivi
+    private boolean inScrittura; // true se c'è uno scrittore attivo
 
-public MasterSincro() {
-    lettoriAttivi = 0;
-    inLettura = false;
-    inScrittura = false;
-}
-public synchronized int inizioLettura() {
-    while (inScrittura == true) {
-        try {
-            wait();
-        }
-        catch (InterruptedException e) {}
+    public MasterSincro() {
+        lettoriAttivi = 0;
+        inLettura = false;
+        inScrittura = false;
     }
-    lettoriAttivi++;
 
-    if (lettoriAttivi == 1)
-        inLettura = true;
+    // Metodo per iniziare una lettura
+    // se c'è uno scrittore, il lettore attende
+    public synchronized int inizioLettura() {
+        while (inScrittura) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                // ignoriamo l'interruzione
+            }
+        }
+        lettoriAttivi++;
+        if (lettoriAttivi == 1)
+            inLettura = true;
+        return lettoriAttivi;
+    }
 
-    return lettoriAttivi;
-}
+    // Metodo per terminare una lettura
+    // se non ci sono più lettori, sveglia eventuali scrittori in attesa
+    public synchronized int fineLettura() {
+        lettoriAttivi--;
+        if (lettoriAttivi == 0)
+            notifyAll();
+        return lettoriAttivi;
+    }
 
-public synchronized int fineLettura() {
-    lettoriAttivi--;
-    if (lettoriAttivi == 0)
+    // Metodo per iniziare una scrittura
+    // attende se ci sono lettori o altri scrittori attivi
+    public synchronized void inizioScrittura() {
+        while (inLettura || inScrittura) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                // ignoriamo l'interruzione
+            }
+        }
+        inScrittura = true;
+    }
+
+    // Metodo per terminare una scrittura
+    // sveglia tutti i thread in attesa
+    public synchronized void fineScrittura() {
+        inScrittura = false;
         notifyAll();
-    return lettoriAttivi;
-}
-
-public synchronized void inizioScrittura() {
-    while (inLettura == true || inScrittura == true){
-        try {
-            wait();
-        }
-        catch (InterruptedException e){}
     }
-    inScrittura = true;
-}
-
-
-public synchronized void fineScrittura() {
-    inScrittura = false;
-    notifyAll();
-}
 }
