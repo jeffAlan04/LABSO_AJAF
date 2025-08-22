497475e46af7fd460b9f385e0d9407cd24d52719
Filippo Bucciarelli <87319226+aka-buccia@users.noreply.github.com>
2025-08-22 16:06:21 +0200

    add: accesso alla lettura

--- src/master/ArbitroLetturaScrittura.java
+++ src/master/ArbitroLetturaScrittura.java
@@ -1,13 +1,31 @@
 public class ArbitroLetturaScrittura {
 
-    private int lettori; // numero di lettori che stanno accedendo
+    private int contatoreLettori; // numero di lettori attivi
     private boolean lettura; // true se almeno un lettore ha l'accesso
     private boolean scrittura; // true se uno scrittore ha l'accesso
 
     public ArbitroLetturaScrittura() {
-        lettori = 0;
+        contatoreLettori = 0;
         lettura = false;
         scrittura = false;
+    }
+
+    public synchronized void inizioLettura() {
+        while (scrittura == false) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+
+            }
+        }
+
+        contatoreLettori++;
+
+        if (readerCount == 1) {
+            lettura = true;
+        }
+
+        // return contatoreLettori;
     }
 
 }
