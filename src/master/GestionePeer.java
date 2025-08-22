740cd46b27ba27884483021c7add9adcb393a3fc
AnouarElKihal <elkihalanouar@gmail.com>
2025-08-22 16:11:05 +0200

    add: costruttore e metodo run

--- src/master/GestionePeer.java
+++ src/master/GestionePeer.java
@@ -1,6 +1,23 @@
 import java.util.*;
 import java.io.*;
+import java.net.*;
 
-public class GestionePeer {
+public class GestionePeer implements Runnable {
+    private final Socket socket;
+    private final ArbitroLetturaScrittura arbitroTabella;
+    private final ArbitroLetturaScrittura arbitroLog;
+    private final GestioneTab gestioneTab;
+
+    public GestionePeer(Socket socket, ArbitroLetturaScrittura arbitroLog, ArbitroLetturaScrittura arbitroTabella, GestioneTab gestioneTab) {
+        this.socket = socket;
+        this.arbitroLog = arbitroLog;
+        this.arbitroTabella = arbitroTabella;
+        this.gestioneTab = gestioneTab;
+    }
+
+    @Override
+    public void run() {
+        // comandi da gestire: listdata remote, quit, add risorsa, download risorsa
+    }
     
 }
