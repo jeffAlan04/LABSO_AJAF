271648008e3cfdaae6778d76dfdc10e4e2af7f2e
jeffAlan04 <jeffalan.shuyinta@studio.unibo.it>
2025-08-24 16:53:45 +0200

    remove: metodo aggiungiRisorsa() vuoto

--- src/client/GestioneRisorse.java
+++ src/client/GestioneRisorse.java
@@ -4,10 +4,6 @@ import java.util.LinkedList;
 
 public class GestioneRisorse {
 
-  public static void aggiungiRisorsa(String nome, String contenuto) {
-
-  }
-
   public static String risorsaPresente(String nome) {
     File directory = new File("risorse");
     File[] risorse = directory.listFiles();
