d76393052c919d9ec15ef118d269b077c217aebf
jeffAlan04 <jeffalan.shuyinta@studio.unibo.it>
2025-08-24 18:57:05 +0200

    add: classe Master.java dal branch feat/master

---
+++ src/master/Master.java
@@ -0,0 +1,45 @@
+import java.util.*;
+import java.io.*;
+import java.net.*;
+
+public class Master{
+
+    private Socket socket;
+    private GestioneTab tabella;
+    private ReaderWriter lettore;
+    private ReaderWriter scrittore;
+    
+    public static boolean inEsecuzione = true;
+
+    public static void main(String[] args) throws IOException {
+        if (args.length < 1){
+            System.out.println("Utilizzo: java Master <porta>");
+            return;
+        }
+            
+            int porta = Integer.parseInt(args[0]);
+
+            tabella = new GestioneTab();
+            lettore = new ReaderWriter();
+            scrittore = new ReaderWriter();
+
+            new Thread(new GestioneComandi(tabella, lettore, scrittore)).start();
+            
+        try (ServerSocket serverSocket = new ServerSocket(porta);){
+        
+            System.out.println("Server in ascolto sulla porta: " + porta);
+
+            while(inEsecuzione){
+                socket = serverSocket.accept();
+                System.out.println("Connessione a: " + socket.getRemoteSocketAddress());
+                GestorePeer gp = new GestorePeer(socket, tabella, lettore, scrittore);
+                new Thread(gp).start();
+            }
+        }
+        
+        catch(IOException e){
+            System.err.println("Errore: " + e.getMessage());
+        }    
+    
+    }
+}
