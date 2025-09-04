29e2a971b0d3b5e861f66a8286de898b89087d78
james <jamesngoufack@gmail.com>
2025-09-04 21:22:08 +0200

    add: Creata struttura iniziale per GestoreComandi (implementa Runnable)

--- src/master/GestoreComandi.java
+++ src/master/GestoreComandi.java
@@ -1,4 +1,13 @@
 package master;
 
-public class GestoreComandi {
+import java.io.*;
+
+public class GestoreComandi implements Runnable{
+    private final ArbitroLetturaScrittura arbitroLog;
+    private final ArbitroLetturaScrittura arbitroTabella;
+
+    public GestoreComandi(ArbitroLetturaScrittura arbitroLog, ArbitroLetturaScrittura arbitroTabella) {
+        this.arbitroLog = arbitroLog;
+        this.arbitroTabella = arbitroTabella;
+    }
 }
