f64b966afdc3974d46e3337b3659668fdf1e080a
jeffAlan04 <jeffalan.shuyinta@studio.unibo.it>
2025-09-04 12:57:18 +0200

    add: metodo per la registrazione locale delle risorse

--- src/client/Client.java
+++ src/client/Client.java
@@ -20,130 +20,178 @@ public class Client {
         Scanner inputMaster = new Scanner(s.getInputStream());
         PrintWriter outputMaster = new PrintWriter(s.getOutputStream());
         Scanner tastiera = new Scanner(System.in);) {
-      System.out.println("Connessione al master: " + s.getRemoteSocketAddress());
+      
+        System.out.println("Connessione al master: " + s.getRemoteSocketAddress());
 
-      avvioServer(); // Avvia PeerServer in contemporanea
+        avvioServer(); // Avvia PeerServer in contemporanea
 
-      // Il client aspetta i comandi dell'utente
-      while (true) {
-        System.out.print("> ");
-        String inputUtente = tastiera.nextLine().trim();
+        registrazioneRisorseLocali();
 
-        if (inputUtente.equalsIgnoreCase("quit")) {
-          eseguiQuit(outputMaster);
-          break;
+        // Il client aspetta i comandi dell'utente
+        while (true) {
+          System.out.print("> ");
+          String inputUtente = tastiera.nextLine().trim();
 
-        }
+          if (inputUtente.equalsIgnoreCase("quit")) {
+            eseguiQuit(outputMaster);
+            break;
 
-        // Esegue il comando listdata local
-       else if (inputUtente.equalsIgnoreCase("listdata local")){
-          GestioneRisorse.eseguiListDataLocal();
-          break;
-       }
-
-       // Esegue il comando add nome_risorsa contenuto
-       else if (inputUtente.startsWith("add ")){
-        String[] parti = inputUtente.split("\\s", 3);
-        if (parti.length < 3){
-          System.out.println("Uso corretto: add nome_risorsa contenuto");
-        }
-        else{
-          String nomeFile = parti[1];
-
-          // Se nome_risorsa inserito dall'utente non contiene ".txt", viene aggiunto.
-          if (!nomeFile.contains(".txt")){
-            nomeFile = nomeFile + ".txt";
-          }
-          String contenuto = parti[2];
-
-          GestioneRisorse.eseguiAdd(nomeFile, contenuto);
-
-          outputMaster.println("ADD " + nomeFile);
-          outputMaster.flush();
-        }
-       }
-
-       // Esegue il comando donwload nome_risorsa.
-       else if (inputUtente.startsWith("download")){
-        String[] parti = inputUtente.split("\\s");
-        if (parti.length != 2){
-          System.out.println("Uso corretto: download nome_risorsa");
-        }
-        else{
-          String nomeRisorsa = parti[1];
-
-          // Se nome_risorsa inserito dall'utente non contiene ".txt", viene aggiunto.
-          if (!nomeRisorsa.contains(".txt")){
-            nomeRisorsa = nomeRisorsa + ".txt";
           }
 
-          outputMaster.println("DOWNLOAD " + nomeRisorsa);
-          outputMaster.flush();
+          // Esegue il comando listdata local
+          else if (inputUtente.equalsIgnoreCase("listdata local")) {
+            GestioneRisorse.eseguiListDataLocal();
+            break;
+          }
 
-          // Legge la risposta del master che contiene l'indirizzo dell'host peer che possiede la risorsa indicata.
-          String indirizzoHostPeer = inputMaster.nextLine();
+          // Esegue il comando add nome_risorsa contenuto
+          else if (inputUtente.startsWith("add ")) {
+            String[] parti = inputUtente.split("\\s", 3);
+            if (parti.length < 3) {
+              System.out.println("Uso corretto: add nome_risorsa contenuto");
+            } else {
+              String nomeFile = parti[1];
 
-          PeerClient pc = new PeerClient(indirizzoHostPeer, porta, nomeRisorsa);
+              // Se nome_risorsa inserito dall'utente non contiene ".txt", viene aggiunto.
+              if (!nomeFile.contains(".txt")) {
+                nomeFile = nomeFile + ".txt";
+              }
+              String contenuto = parti[2];
 
-          boolean risorsaTrovata = pc.avviaConnessione();
+              GestioneRisorse.eseguiAdd(nomeFile, contenuto);
 
-          // Fino a che risorsaTrovata non corrisponde a true, il master deve fornire un host peer alternativo, a meno che questo sia "NESSUNO" uscendo dal ciclo.
-          while (!risorsaTrovata){
-            outputMaster.println("Risorsa non disponibile dal Peer: " + indirizzoHostPeer);
+              outputMaster.println("ADD " + nomeFile);
+              outputMaster.flush();
+            }
+          }
+
+          // Esegue il comando donwload nome_risorsa.
+          else if (inputUtente.startsWith("download")) {
+            String[] parti = inputUtente.split("\\s");
+            if (parti.length != 2) {
+              System.out.println("Uso corretto: download nome_risorsa");
+            } else {
+              String nomeRisorsa = parti[1];
+
+              // Se nome_risorsa inserito dall'utente non contiene ".txt", viene aggiunto.
+              if (!nomeRisorsa.contains(".txt")) {
+                nomeRisorsa = nomeRisorsa + ".txt";
+              }
+
+              outputMaster.println("DOWNLOAD " + nomeRisorsa);
+              outputMaster.flush();
+
+              // Legge la risposta del master che contiene l'indirizzo dell'host peer che
+              // possiede la risorsa indicata.
+              String indirizzoHostPeer = inputMaster.nextLine();
+
+              PeerClient pc = new PeerClient(indirizzoHostPeer, porta, nomeRisorsa);
+
+              boolean risorsaTrovata = pc.avviaConnessione();
+
+              // Fino a che risorsaTrovata non corrisponde a true, il master deve fornire un
+              // host peer alternativo, a meno che questo sia "NESSUNO" uscendo dal ciclo.
+              while (!risorsaTrovata) {
+                outputMaster.println("Risorsa non disponibile dal Peer: " + indirizzoHostPeer);
+                outputMaster.flush();
+
+                String indirizzoHostPeerAlternativo = inputMaster.nextLine();
+
+                if (indirizzoHostPeerAlternativo.equals("NESSUNO")) {
+                  System.out.println("Download fallito: nessun peer disponibile");
+                  break;
+                } else {
+                  PeerClient pcAlternativo = new PeerClient(indirizzoHostPeerAlternativo, porta, nomeRisorsa);
+
+                  risorsaTrovata = pcAlternativo.avviaConnessione();
+                }
+
+              }
+
+            }
+
+          }
+
+          else if (inputUtente.equalsIgnoreCase("listdata remote")) {
+            outputMaster.println("LISTDATA_REMOTE");
             outputMaster.flush();
 
-            String indirizzoHostPeerAlternativo = inputMaster.nextLine();
-
-            if (indirizzoHostPeerAlternativo.equals("NESSUNO")){
-              System.out.println("Download fallito: nessun peer disponibile");
-              break;
+            if (inputMaster.hasNextLine()) {
+              String risposta = inputMaster.nextLine();
+              GestioneRisorse.eseguiListDataRemote(risposta);
             }
-            else{
-              PeerClient pcAlternativo = new PeerClient(indirizzoHostPeerAlternativo, porta, nomeRisorsa);
 
-              risorsaTrovata = pcAlternativo.avviaConnessione();
+            else {
+              System.out.println("Nessuna risposta ricevuta");
             }
-            
           }
-          
+
+          // Da togliere non appena completati tutti i comandi
+          else {
+            System.out.println("Altri comandi");
+          }
+
         }
-        
-       }
+      } catch (IOException e) {
+        System.out.println("Errore di connessione al master");
+      }
+    }
 
-       else if (inputUtente.equalsIgnoreCase("listdata remote")){
-          outputMaster.println("LISTDATA_REMOTE");
-          outputMaster.flush();
+    public static List<String> registrazioneRisorseLocali() {
+      List<String> risorseLocali = new ArrayList<>();
 
-          if (inputMaster.hasNextLine()){
-            String risposta = inputMaster.nextLine();
-            GestioneRisorse.eseguiListDataRemote(risposta);
+      String indirizzoPeer = null;
+
+      try {
+        // Ottiene l'indirizzo IP locale
+        indirizzoPeer = InetAddress.getLocalHost().getHostAddress();
+      }
+
+      catch (UnknownHostException e) {
+        System.err.println("Errore nel recuper dell'indirizzo IP: " + e.getMessage());
+      }
+
+      risorseLocali.add("REGISTRAZIONE RISORSE " + indirizzoPeer);
+
+      // All'interno della cartella risorse, vengono aggiunti alla lista.
+      File cartella = new File("src/client/risorse");
+      if (cartella.exists() && cartella.isDirectory()) {
+        File[] f = cartella.listFiles();
+
+        if (f != null) {
+
+          for (File file : f) {
+
+            if (file.isFile()) {
+
+              risorseLocali.add("- " + file.getName());
+
+            }
+
           }
-          
-          else{
-            System.out.println("Nessuna risposta ricevuta");
-          }
-       }
-       
-       // Da togliere non appena completati tutti i comandi
-       else{
-        System.out.println("Altri comandi");
-       }
+
+        }
 
       }
-    } catch (IOException e) {
-      System.out.println("Errore di connessione al master");
+
+      else {
+        System.out.println("cartella risorse non trovata");
+      }
+
+      risorseLocali.add("FINE");
+
+      return risorseLocali;
     }
-  }
 
-  // Metodo per il comando quit
-  private static void eseguiQuit(PrintWriter outputMaster) {
-    outputMaster.println("QUIT");
-    outputMaster.flush();
-    System.out.println("Disconnessione in corso");
+    // Metodo per il comando quit
+    private static void eseguiQuit(PrintWriter outputMaster) {
+      outputMaster.println("QUIT");
+      outputMaster.flush();
+      System.out.println("Disconnessione in corso");
 
-    server.terminaServer(); // termina PeerServer
+      server.terminaServer(); // termina PeerServer
 
-  }
+    }
 
   // Crea un thread che esegua PeerServer
   private static void avvioServer() {
