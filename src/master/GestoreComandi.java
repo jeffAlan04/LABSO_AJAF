d60827ae86cab301d7bd7ef756353e4e16ceba42
james <jamesngoufack@gmail.com>
2025-09-04 21:56:41 +0200

    add: Implementata logica di esecuzione in GestoreComandi, Aggiunta di (LOG, LISTDATA REMOTE, QUIT)

--- src/master/GestoreComandi.java
+++ src/master/GestoreComandi.java
@@ -10,4 +10,32 @@ public class GestoreComandi implements Runnable{
         this.arbitroLog = arbitroLog;
         this.arbitroTabella = arbitroTabella;
     }
+
+    @Override
+    public void run() {
+        try (
+                BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+                PrintWriter out = new PrintWriter(System.out, true);
+        ) {
+            String messaggio;
+            while ((messaggio = in.readLine()) != null) {
+                messaggio = messaggio.trim();
+
+                if (messaggio.toUpperCase().startsWith("LOG")) {
+                    gestisciLog(out); // aggiungi risorsa
+                } else if (messaggio.equalsIgnoreCase("LISTDATA REMOTE")) {
+                    gestisciListData(out); // lista risorse remote
+                } else if (messaggio.equalsIgnoreCase("QUIT")) {
+                     // segnala disconnessione
+                    out.println("Peer " + "(aggiungere nome peer)"  + " disconnesso");
+                    //aggiungere comando per chiudere anche tutti i programmi del master
+                    break;
+                }  else {
+                    out.println("Comando non riconosciuto: " + messaggio);
+                }
+            }
+        } catch (IOException e) {
+            System.err.println("Errore con peer " + "(aggiungere nome peer)" + ": " + e.getMessage());
+        }
+    }
 }
