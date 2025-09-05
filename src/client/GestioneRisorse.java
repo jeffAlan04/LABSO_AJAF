9cd0f4c4b3c2dbdc9ca4b3eef190647d6338f62f
Filippo Bucciarelli <87319226+aka-buccia@users.noreply.github.com>
2025-09-05 14:56:54 +0200

    add: metodo per controllo presenza cartella risorse

--- src/client/GestioneRisorse.java
+++ src/client/GestioneRisorse.java
@@ -4,55 +4,65 @@ import java.util.LinkedList;
 
 public class GestioneRisorse {
 
-  public static String risorsaPresente(String nome) {
-    File directory = new File("risorse");
-    File[] risorse = directory.listFiles();
+    // Metodo per verificare la presenza di una risorsa all'interno della cartella
+    // risorse
+    public static String risorsaPresente(String nome) {
+        controlloCartella();
 
-    if (risorse != null && risorse.length != 0) {
-      for (File risorsa : risorse) {
-        if (nome.equals(risorsa.getName())) {
-          return "true";
+        File directory = new File("risorse");
+        File[] risorse = directory.listFiles();
+
+        if (risorse != null && risorse.length != 0) {
+            for (File risorsa : risorse) {
+                if (nome.equals(risorsa.getName())) {
+                    return "true";
+                }
+            }
         }
-      }
+
+        return "false";
     }
 
-    return "false";
-  }
-// Metodo per il comando listdata local
-  public static void eseguiListDataLocal(){
-    File input = new File("risorse");
-    File[] risorse = input.listFiles();
+    // Metodo per il comando listdata local
+    public static void eseguiListDataLocal() {
+        controlloCartella();
 
-    if (risorse == null || risorse.length == 0){
-      System.out.println("Nessuna risorsa disponibile");
-      return;
+        File input = new File("risorse");
+        File[] risorse = input.listFiles();
+
+        if (risorse == null || risorse.length == 0) {
+            System.out.println("Nessuna risorsa disponibile");
+            return;
+        }
+        System.out.println("Risorse: ");
+        for (File f : risorse) {
+            if (f.isFile()) {
+                System.out.println("- " + f.getName());
+            }
+        }
     }
-    System.out.println("Risorse: ");
-    for (File f : risorse) {
-      if(f.isFile()){
-        System.out.println("- " + f.getName());
-      }
+
+    // Metodo per il comando add <nome risorsa> <contenuto>
+    public static void eseguiAdd(String nomeFile, String contenuto) {
+        try {
+            controlloCartella();
+
+            File nuovoFile = new File(cartella, nomeFile);
+
+            FileWriter writer = new FileWriter(nuovoFile);
+            writer.write(contenuto);
+            writer.close();
+
+            System.out.println("File: " + nomeFile + " creato con successo");
+        } catch (Exception e) {
+            System.out.println("Errore nella creazione della risorsa");
+        }
     }
-  }
 
-//Metodo per il comando add <nome risorsa> <contenuto>
-  public static void eseguiAdd(String nomeFile, String contenuto){
-    try {
-      File cartella = new File("src/client/risorse");
-      if (!cartella.exists()){
-        cartella.mkdirs();
-      }
-
-      File nuovoFile = new File(cartella, nomeFile);
-
-      FileWriter writer = new FileWriter(nuovoFile);
-      writer.write(contenuto);
-      writer.close();
-
-      System.out.println("File: " + nomeFile + " creato con successo");
-    } 
-    catch (Exception e) {
-      System.out.println("Errore nella creazione della risorsa");
+    private static void controlloCartella() {
+        File cartella = new File("risorse");
+        if (!cartella.exists()) {
+            cartella.mkdirs();
+        }
     }
-  }
 }
