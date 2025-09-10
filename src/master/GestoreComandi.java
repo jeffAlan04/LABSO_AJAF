79a044684be46244f6852f00e079eefe35422c6e
james <jamesngoufack@gmail.com>
2025-09-11 01:46:49 +0200

    Revert "fix: risolto stampa"
    This reverts commit 912f458c43ff70df8568ec4a91d1809c75530472.

--- src/master/GestoreComandi.java
+++ src/master/GestoreComandi.java
@@ -28,7 +28,8 @@ public class GestoreComandi implements Runnable {
         try (
                 BufferedReader in = new BufferedReader(new InputStreamReader(System.in));) {
             String messaggio;
-            while ((messaggio = in.readLine()) != null) {
+            while ((messaggio = in.readLine()) != null){
+                System.out.print("> ");
                 // rimuovo gli eventuali spazi e rendo tutta la stringa in maiuscolo
                 messaggio = messaggio.trim().toUpperCase();
 
