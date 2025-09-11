ec8f95872af96a9d696c0e078162fc7d6b9d3769
Filippo Bucciarelli <87319226+aka-buccia@users.noreply.github.com>
2025-09-11 12:47:25 +0200

    add: creazione cartella log nel caso non esista

---
+++ src/master/Logger.java
@@ -0,0 +1,92 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.io.File;
+
+public class Logger {
+    private PrintWriter scrittore;
+    private String classe;
+    private final String CARTELLA_LOG = ".log/";
+
+    public Logger(String classe) {
+        this.classe = classe;
+
+        PrintWriter scrittoreTemporaneo;
+        try {
+            controlloEsistenzaCartellaLog();
+            FileWriter f = new FileWriter(new File(generaNome()), true);
+            scrittoreTemporaneo = new PrintWriter(f);
+        } catch (IOException e) {
+            System.out.println("Errore nella creazione del file di log. I loge verranno stampati su console");
+            scrittoreTemporaneo = new ConsolePrintWriter();
+        }
+
+        this.scrittore = scrittoreTemporaneo;
+    }
+
+    private void controlloEsistenzaCartellaLog() {
+        File cartella = new File(CARTELLA_LOG);
+        if (!cartella.exists()) {
+            cartella.mkdirs();
+        }
+    }
+
+    private String generaNome() {
+        LocalDateTime momento = LocalDateTime.now();
+        DateTimeFormatter formattatore = DateTimeFormatter.ofPattern("yyyy-MM-dd_HH-mm-ss");
+        String dataOra = momento.format(formattatore);
+
+        // Formato: <nome_classe>_<identificativo>_<data>_<ora>.log
+        return CARTELLA_LOG + classe + "_" + dataOra + ".log";
+    }
+
+    public static String stampaMomento() {
+        LocalDateTime momento = LocalDateTime.now();
+        DateTimeFormatter formattatore = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        String dataOra = momento.format(formattatore);
+        return dataOra;
+    }
+
+    public void log(String messaggio) {
+        String messaggioFinale = stampaMomento() + " " + messaggio;
+        scrittore.println(messaggioFinale);
+        scrittore.flush();
+    }
+
+    public void logErrore(String messaggio) {
+        messaggio = "[ERRORE]\t" + messaggio;
+        log(messaggio);
+    }
+
+    public void logInfo(String messaggio) {
+        messaggio = "[INFO]\t" + messaggio;
+        log(messaggio);
+    }
+
+    public void close() {
+        if (scrittore != null)
+            scrittore.close();
+    }
+
+    // ConsolePrintWriter è una estensione di PrintWriter che scrive i log su
+    // console
+    private static class ConsolePrintWriter extends PrintWriter {
+        ConsolePrintWriter() {
+            super(new OutputStreamWriter(System.out));
+        }
+
+        @Override
+        public void close() {
+        } // Metodo no-op perchè non c'è nulla da chiudere
+    }
+
+    // da eliminare, solo per testing
+    public static void main(String[] args) {
+        Logger prova = new Logger("Logger");
+        prova.logInfo("Sto facendo una prova");
+        prova.close();
+    }
+}
