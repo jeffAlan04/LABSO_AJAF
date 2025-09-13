f85d41bdfaf2de8f2e7223cb9e305be4b7da458a
Filippo Bucciarelli <87319226+aka-buccia@users.noreply.github.com>
2025-09-13 18:11:06 +0200

    fix: rimosse istruzioni non utilizzate

--- src/client/PeerServer.java
+++ src/client/PeerServer.java
@@ -18,15 +18,15 @@ public class PeerServer implements Runnable {
     private Logger logger;
     private final String CARTELLA_RISORSE = "risorse/";
 
-    public PeerServer(int porta) {
-        this.porta = porta;
+    public PeerServer() {
         this.logger = new Logger("PeerServer");
     }
 
     @Override
     public void run() {
         try {
-            this.serverSocket = new ServerSocket(porta);
+            this.serverSocket = new ServerSocket(0);
+            this.porta = serverSocket.getLocalPort();
             logger.logInfo("Server in ascolto sulla porta " + porta);
             running = true;
 
@@ -95,6 +95,10 @@ public class PeerServer implements Runnable {
         } catch (IOException e) {
             logger.logErrore("Errore durante upload della risorsa " + nomeRisorsa);
         }
+    }
+
+    public int getPorta() {
+        return this.porta;
     }
 
     public void terminaServer() {
